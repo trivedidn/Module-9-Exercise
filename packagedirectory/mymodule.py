1d6e606d4732512cd21aa74d93e8f1df009fb165
trivedidn <svw6dn@virginia.edu>
2024-07-02 11:29:46 -0400

    All files

---
+++ packagedirectory/mymodule.py
@@ -0,0 +1,2 @@
+def create_something():
+    print("Something new")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Module 9 Exercises',
+    version='0.1',
+    url='https://github.com/trivedidn/Module-9-Exercise',
+    author='Disha Trivedi',
+    author_email='svw6dn@virginia.edu',
+    description='Package for Module 9 Exercise',
+    packages=['packagedirectory'], 
+)
